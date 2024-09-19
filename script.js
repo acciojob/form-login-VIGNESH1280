a61b5faa863bdcb4ef6fe6aeddf20bf28d42fc5c
C.Vignesh <162482102+VIGNESH1280@users.noreply.github.com>
2024-09-19 10:49:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 function getFormvalue() {
-    //Write your code here
+ //    //Write your code here
+	// var form = document.getElementById('form1');
+
+	// var firstName = form.elements['fnmae'].value;
+	// var lastName = form.elements['lnmae'].value;
+	//  alert(firstName + " " + lastName);
+
+
+	
+function getFormvalue() {
+
+var form = document.getElementById('form1');
+}
+// Get first and last name values
+var firstName = form.elements['fname'].value; 
+var lastName = form.elements ['lname'].value;
+// Display alert with full name
+alert(firstName + " " + lastName);
+	
 
 }
